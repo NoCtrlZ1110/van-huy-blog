73e9daaba8b8f4f6e87ef333e8407136af994fdd
Nguyễn Văn Huy <huy.nv@teko.vn>
2024-01-19 01:12:00 +0700

    feat: setup my first blog

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,25 +1,25 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: "Văn Huy's Blog",
+  author: 'Nguyễn Văn Huy',
+  headerTitle: "Văn Huy's Blog ",
+  description: 'My personal blog which I write about my life, my work and my hobbies.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://vanhuy.dev/',
+  siteRepo: 'https://github.com/NoCtrlZ1110/van-huy-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  locale: 'en-US',
+  email: 'nguyen@vanhuy.dev',
+  github: 'https://github.com/NoCtrlZ1110',
+  twitter: 'https://twitter.com/huynd_nguyen',
+  facebook: 'https://facebook.com/NoCtrlZ',
+  youtube: 'https://www.youtube.com/@vanhuydev',
+  linkedin: 'https://www.linkedin.com/in/noctrlz/',
+  threads: '',
+  instagram: 'https://www.instagram.com/huynd.nguyen/',
+  locale: 'vi-VN',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
@@ -35,9 +35,9 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
+    googleAnalytics: {
+      googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, // e.g. G-XXXXXXX
+    },
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.google-analytics.com www.googletagmanager.com https://analytics.google.com/;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
+  media-src 'none';
   connect-src *;
   font-src 'self';
   frame-src giscus.app
